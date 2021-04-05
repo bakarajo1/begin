4fe632e299f24a3d47d101e35e03cc5dedca5429
bakarajo <bakar.zhghenti.1@btu.edu.ge>
2021-04-05 16:47:51 +0400

    init

---
+++ app/src/main/java/com/example/beginbtu/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.beginbtu;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
